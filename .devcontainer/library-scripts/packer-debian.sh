7659b15efe6dfa52a8b5058910c030cb076ead1b
Jim Counts <jamesrcounts@outlook.com>
2020-12-28 06:45:29 -0800

    Update packer-debian.sh

--- .devcontainer/library-scripts/packer-debian.sh
+++ .devcontainer/library-scripts/packer-debian.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
+apt-get update && \
+    apt-get install software-properties-common
+    
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 apt-get update && \
