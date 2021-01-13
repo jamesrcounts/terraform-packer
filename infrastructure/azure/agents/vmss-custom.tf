20e03a194bfe0cdad7a16a0e2c830e3f050493ed
Jim Counts <jamesrcounts@outlook.com>
2021-01-13 14:11:59 +0000

    Disable experimental vmss

--- infrastructure/azure/agents/vmss-custom.tf
+++ infrastructure/azure/agents/vmss-custom.tf
@@ -12,4 +12,27 @@ module "vmss-custom" {
     workspace_id       = data.azurerm_key_vault_secret.secrets["log-analytics-workspace-id"].value
     primary_shared_key = data.azurerm_key_vault_secret.secrets["log-analytics-workspace-key"].value
   }
-}       
+}
+
+# module "vmss-custom2" {
+#   source = "github.com/jamesrcounts/terraform-packer.git//infrastructure/azure/modules/vmss"
+
+#   disk_encryption_set_id = data.azurerm_key_vault_secret.secrets["disk-encryption-set-id"].value
+#   project                = "${local.project}-custom2"
+#   resource_group         = data.azurerm_resource_group.main
+#   # source_image_id        = azurerm_image.agents["pkrosma1pn5fgmm"].id
+#   subnet_id = data.azurerm_key_vault_secret.secrets["agents-subnet-id"].value
+#   tags      = local.tags
+
+#   source_image_reference = {
+#     publisher = "Canonical"
+#     offer     = "UbuntuServer"
+#     sku       = "18.04-LTS"
+#     version   = "latest"
+#   }
+
+#   log_analytics_workspace = {
+#     workspace_id       = data.azurerm_key_vault_secret.secrets["log-analytics-workspace-id"].value
+#     primary_shared_key = data.azurerm_key_vault_secret.secrets["log-analytics-workspace-key"].value
+#   }
+# }       
