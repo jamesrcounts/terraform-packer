8f3a89787ffd2d833273344a264a3d0f1be16f36
Jim Counts <jamesrcounts@outlook.com>
2021-01-31 22:21:47 +0000

    Update vm sku, roll image

--- infrastructure/azure/agents/vmi.tf
+++ infrastructure/azure/agents/vmi.tf
@@ -1,9 +1,21 @@
 locals {
   agent_images = {
-    "pkros1v5nlqoh4g" = "https://saterraformpackerpackern.blob.core.windows.net/images/pkros1v5nlqoh4g.vhd"
-    "pkrosfccg6m89e7" = "https://saterraformpackerpackern.blob.core.windows.net/images/pkrosfccg6m89e7.vhd"
-    "pkrosv7zehc5jio" = "https://saterraformpackerpackern.blob.core.windows.net/images/pkrosv7zehc5jio.vhd"
-    "pkrdpm1d19vntby" = "https://saterraformpackerpackern.blob.core.windows.net/images/pkrdpm1d19vntby.vhd"
+    "pkros1v5nlqoh4g" = {
+      blob_uri = "https://saterraformpackerpackern.blob.core.windows.net/images/pkros1v5nlqoh4g.vhd"
+      size_gb  = 86
+    }
+    "pkrosfccg6m89e7" = {
+      blob_uri = "https://saterraformpackerpackern.blob.core.windows.net/images/pkrosfccg6m89e7.vhd"
+      size_gb  = 86
+    }
+    "pkrosv7zehc5jio" = {
+      blob_uri = "https://saterraformpackerpackern.blob.core.windows.net/images/pkrosv7zehc5jio.vhd"
+      size_gb  = 86
+    }
+    "pkrosm1d19vntby" = {
+      blob_uri = "https://saterraformpackerpackern.blob.core.windows.net/images/pkrosm1d19vntby.vhd",
+      size_gb  = 100
+    }
   }
 }
 
@@ -18,7 +30,7 @@ resource "azurerm_image" "agents" {
   os_disk {
     os_type  = "Linux"
     os_state = "Generalized"
-    blob_uri = each.value
-    size_gb  = 100 // TODO support by sku?
+    blob_uri = each.value.blob_uri
+    size_gb  = each.value.size_gb
   }
 }
