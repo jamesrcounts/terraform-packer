9d6952fb97b7850038a500fa3adea6887cdc24e3
Jim Counts <jamesrcounts@outlook.com>
2021-02-28 22:31:46 +0000

    2nd bootstrap pool (temp)

--- infrastructure/azure/base/vmss-bootstrap.tf
+++ infrastructure/azure/base/vmss-bootstrap.tf
@@ -28,3 +28,26 @@ locals {
   log_analytics_workspace_id  = azurerm_log_analytics_workspace.insights.workspace_id
   log_analytics_workspace_key = azurerm_log_analytics_workspace.insights.primary_shared_key
 }
+
+module "vmss-custom2" {
+  source = "github.com/jamesrcounts/terraform-packer.git//infrastructure/azure/modules/vmss"
+
+  disk_encryption_set_id = local.disk_encryption_set_id
+  project                = "${local.project}-bootstrap2"
+  resource_group         = local.agents_resource_group
+  sku                    = "Standard_D4ds_v4"
+  subnet_id              = local.agents_subnet_id
+  tags                   = local.tags
+
+  source_image_reference = {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18_04-lts-gen2"
+    version   = "latest"
+  }
+
+  log_analytics_workspace = {
+    workspace_id       = local.log_analytics_workspace_id
+    primary_shared_key = local.log_analytics_workspace_key
+  }
+}
