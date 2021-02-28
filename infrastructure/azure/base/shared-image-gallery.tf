c8494d8076371f53d7548f317b74e32fe1d1c36f
Jim Counts <jamesrcounts@outlook.com>
2021-02-28 00:30:35 +0000

    Add custom image

--- infrastructure/azure/base/shared-image-gallery.tf
+++ infrastructure/azure/base/shared-image-gallery.tf
@@ -23,3 +23,21 @@ resource "azurerm_shared_image" "bootstrap" {
     sku       = "bootstrap"
   }
 }
+
+resource "azurerm_shared_image" "custom" {
+  description         = "Custom build agent."
+  gallery_name        = azurerm_shared_image_gallery.images.name
+  hyper_v_generation  = "V2"
+  location            = azurerm_resource_group.images.location
+  name                = "custom"
+  os_type             = "Linux"
+  resource_group_name = azurerm_resource_group.images.name
+  specialized         = false
+  tags                = local.tags
+
+  identifier {
+    offer     = "Custom"
+    publisher = local.project
+    sku       = "custom"
+  }
+}
